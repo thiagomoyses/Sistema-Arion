6635ffbd7ce72b2f42514cbf33ac4518d69d2b0d
THIAGO MOYSES <thiago-_-moyses@hotmail.com>
2020-10-19 21:30:25 -0300

    Add files via upload

---
+++ acomp/menu.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<link href="assets/css/bootstrap.css" rel="stylesheet">
+<link href="assets/css/css_menu.css" rel="stylesheet">
+</head>
+
+<body>
+<div class="container menu-container">
+            <div class="row">
+                <div class="col-md-6 menu-form-1">
+                    <h3>EDITAR ACOMPANHAMENTOS:</h3>
+                    <a href='edit_chamado.php'><img src="assets/images/menu/acompanhamentos.png"width="100%"></a>
+                </div>
+                <div class="col-md-6 menu-form-2">
+                    <h3>UPLOADS DE GMUDS: </h3>
+                    <img src="assets/images/menu/gmuds.png" width="100%">
+                </div>
+            </div>
+        </div>
+</body>
+</html>
